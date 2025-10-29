9b2a65a29f0667259fd4f6df83bca95fe09fa63b
Saugat Rasaili <saugatrasaili76@gmail.com>
2025-10-29 21:58:17 +0545

    edited main.java file

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
-package src;
+import java.util.Scanner;
 
 public class Main {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+
+       
+            System.out.println("\n==== College Program Management System ====");
+            System.out.println("1. Add Event");
+            System.out.println("2. Register Participant");
+            System.out.println("3. Schedule Event");
+            System.out.println("4. View Event Participants");
+            System.out.println("5. Manage Results");
+            System.out.println("6. View Scoreboard");
+            System.out.println("7. Generate Report");
+            System.out.println("8. Exit");
+
+            System.out.print("Enter choice: ");
+            
+
+
+   
+              
+                
+       
 }
