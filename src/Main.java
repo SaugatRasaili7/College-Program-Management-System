80ed90b679c29ab5a70f0ae1af07b1051d9dee10
Saugat Rasaili <saugatrasaili76@gmail.com>
2025-10-31 12:51:54 +0545

    Edited a file

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,37 @@
-package src;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        
 
-       
-            System.out.println("\n==== College Program Management System ====");
+        System.out.println("\n==== College Program Management System ====");
+        System.out.println("1. Add Event");
+        System.out.println("2. Register Participant");
+        System.out.println("3. Schedule Event");
+        System.out.println("4. View Event Participants");
+        System.out.println("5. Manage Results");
+        System.out.println("6. View Scoreboard");
+        System.out.println("7. Generate Report");
+        System.out.println("8. Exit");
+
+        System.out.print("Enter choice: ");
+        int num = sc.nextInt();
+        sc.nextLine(); 
+
+        if (num == 1) {
             System.out.println("1. Add Event");
-            System.out.println("2. Register Participant");
-            System.out.println("3. Schedule Event");
-            System.out.println("4. View Event Participants");
-            System.out.println("5. Manage Results");
-            System.out.println("6. View Scoreboard");
-            System.out.println("7. Generate Report");
-            System.out.println("8. Exit");
+            System.out.print("Please enter the event name: ");
+            String eventName = sc.nextLine();
+            System.out.println("Event " + eventName + " added successfully!");
 
-            System.out.print("Enter choice: ");
+        }
+    else if(num==2){
+          System.out.println("2. Add Participant");
+            System.out.print("Please enter the Participant's name: ");
+            String pname = sc.nextLine();
+            System.out.println("Participant" + pname + " added successfully!");
 
-
-
-   
-              
-                
-       
+    }
+        sc.close();
+    }
 }
