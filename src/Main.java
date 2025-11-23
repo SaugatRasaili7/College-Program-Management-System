8cf121ce9d131727f02753d27d991d9e31ef4019
Saugat Rasaili <saugatrasaili76@gmail.com>
2025-11-23 14:27:00 +0545

    Your message here

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,14 @@
 public class Main {
-    
+      System.out.println(" College Program Management System");
+            System.out.println("1. Add Event");
+            System.out.println("2. Register Participant");
+            System.out.println("3. Schedule / Reschedule Event");
+            System.out.println("4. Register Participant to Event");
+            System.out.println("5. View Event-wise Participants");
+            System.out.println("6. Enter Results (1st/2nd/3rd)");
+            System.out.println("7. View Scoreboard / Rankings");
+            System.out.println("8. Search & Filter (events or participants)");
+            System.out.println("9. Generate / Print Event Report (console & file)");
+            System.out.println("0. Exit");
+            System.out.print("Choose: ");
 }
